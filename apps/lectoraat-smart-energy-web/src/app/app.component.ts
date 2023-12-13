f08f4095364e1d8cc2e443e5d754679b470575ee
studentThomas <twa.vermeulen@student.avans.nl>
2023-12-13 10:38:42 +0100

    feat: add boiler component

--- apps/lectoraat-smart-energy-web/src/app/app.component.ts
+++ apps/lectoraat-smart-energy-web/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {LayoutModule} from "@lectoraat-smart-energy/ui/layout";
+import { LayoutModule } from '@lectoraat-smart-energy/ui/layout';
+import { BoilerComponent } from '@lectoraat-smart-energy/ui/pages';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, LayoutModule],
+  imports: [RouterModule, LayoutModule, BoilerComponent],
   selector: 'lectoraat-smart-energy-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
