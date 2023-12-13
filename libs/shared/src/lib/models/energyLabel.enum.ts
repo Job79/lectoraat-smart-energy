2d42134ece1ed96d6dc57ddcde4d7d8d567aac3c
studentThomas <twa.vermeulen@student.avans.nl>
2023-12-13 11:35:12 +0100

    feat: add domains

---
+++ libs/shared/src/lib/models/energyLabel.enum.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-duplicate-enum-values */
+export enum EnergyLabel {
+  'A+' = 22.79,
+  'A' = 31.75,
+  'B' = 44.44,
+  'C' = 62.23,
+  'D' = 77.51,
+  'E' = 110.73,
+  'F' = 122.14,
+  'G' = 122.14,
+}
--- libs/ui/pages/src/lib/calculator/boiler/boiler.component.ts
+++ libs/ui/pages/src/lib/calculator/boiler/boiler.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BoilerService } from '../boiler.servise';
 
 @Component({
   selector: 'lectoraat-smart-energy-boiler',
   standalone: true,
   imports: [CommonModule],
+  providers: [BoilerService],
   templateUrl: './boiler.component.html',
   styleUrl: './boiler.component.css',
 })
