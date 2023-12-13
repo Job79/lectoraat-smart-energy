e28c63148c8fcc53ceeb10969f8a351f83022f78
studentThomas <twa.vermeulen@student.avans.nl>
2023-12-13 10:38:42 +0100

    feat: add boiler component

--- apps/lectoraat-smart-energy-web/src/app/app.routes.ts
+++ apps/lectoraat-smart-energy-web/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
+import { BoilerComponent } from '@lectoraat-smart-energy/ui/pages';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'calculator',
+    children: [
+      {
+        path: 'boiler',
+        component: BoilerComponent,
+      },
+    ],
+  },
+];
---
+++ libs/ui/pages/src/lib/calculator/boiler/boiler.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lectoraat-smart-energy-boiler',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './boiler.component.html',
+  styleUrl: './boiler.component.css',
+})
+export class BoilerComponent {}
