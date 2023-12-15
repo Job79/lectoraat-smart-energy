28dabd99c1c1705486b89803d7ba0e17d7f05414
Job79 <job@plabble.org>
2023-12-15 10:02:56 +0100

    refactor: remove BoilerComponent import

--- apps/lectoraat-smart-energy-web/src/app/app.component.ts
+++ apps/lectoraat-smart-energy-web/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LayoutModule } from '@lectoraat-smart-energy/ui/layout';
-import { BoilerComponent } from '@lectoraat-smart-energy/ui/pages';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, LayoutModule, BoilerComponent],
+  imports: [RouterModule, LayoutModule],
   selector: 'lectoraat-smart-energy-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
