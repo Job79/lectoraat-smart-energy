40ff3fbea68d05de47d2a4da64eee11eaafedeca
studentThomas <twa.vermeulen@student.avans.nl>
2023-12-17 13:15:20 +0100

    feat: add user domain

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/models/boiler.interface';
 export * from './lib/models/energyLabels';
+export * from './lib/models/user.interface';
 export * from './lib/models/pocketbase.interface';
