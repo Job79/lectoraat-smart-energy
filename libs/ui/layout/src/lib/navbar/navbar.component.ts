5789a500f821a916fb74c32c70d93165f154a543
studentThomas <twa.vermeulen@student.avans.nl>
2023-12-18 10:02:23 +0100

    feat: updated navbar

--- libs/ui/layout/src/lib/navbar/navbar.component.ts
+++ libs/ui/layout/src/lib/navbar/navbar.component.ts
@@ -1,26 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { User } from '@lectoraat-smart-energy/shared';
 import { AuthService } from '@lectoraat-smart-energy/ui/auth';
 
 @Component({
   selector: 'lectoraat-smart-energy-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   providers: [AuthService],
   templateUrl: './navbar.component.html',
   styles: [],
 })
 export class NavbarComponent implements OnInit {
   imagePath?: string;
+  user?: User;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.imagePath = 'assets/logo-home.svg';
+    this.authService.getUser().then((user) => {
+      this.user = user;
+    });
   }
 
   logout(): void {
-    console.log('logout');
     this.authService.logout();
   }
 }
