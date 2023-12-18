6a0b7b9162bc4f4f20ded9e01f1c6cb717b81261
studentThomas <twa.vermeulen@student.avans.nl>
2023-12-18 10:02:23 +0100

    feat: updated navbar

--- libs/ui/auth/src/lib/auth.service.ts
+++ libs/ui/auth/src/lib/auth.service.ts
@@ -10,33 +10,28 @@ export class AuthService {
   constructor(private router: Router) {}
 
   public async login(user: User) {
-    console.log('Login attempt:', user);
-
     try {
-      const authData = await this.pb
+      await this.pb
         .collection('users')
         .authWithPassword(user.email, user.password);
-      console.log('Result:', authData);
       this.router.navigate(['/']);
     } catch (error) {
       console.error(error);
       try {
-        const authData = await this.pb.admins.authWithPassword(
-          user.email,
-          user.password,
-        );
-        console.log('Result:', authData);
+        await this.pb.admins.authWithPassword(user.email, user.password);
         this.router.navigate(['/']);
       } catch (error) {
         console.error(error);
       }
     }
-    // console.log((this.pb.authStore.model as User)?.id);
   }
 
   public async logout() {
-    console.log('Logout');
     await this.pb.authStore.clear();
     this.router.navigate(['/auth/login']);
+  }
+
+  public async getUser(): Promise<User> {
+    return (await this.pb.authStore.model) as User;
   }
 }
