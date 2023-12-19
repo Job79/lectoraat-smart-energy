6920531be5d7b9134a355576a9cc8e5058aefbe8
Job79 <job@plabble.org>
2023-12-19 21:10:52 +0100

    fix: pocketbase url for prod

--- frontend/libs/shared/src/lib/env/environment.production.ts
+++ frontend/libs/shared/src/lib/env/environment.production.ts
@@ -2,5 +2,5 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
   production: true,
-  pocketbase: 'http://127.0.0.1:8090',
+  pocketbase: '/',
 };
