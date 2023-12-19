baed95871100361e599b274981845157e42c1b68
studentThomas <twa.vermeulen@student.avans.nl>
2023-12-19 10:05:10 +0100

    feat: add get users

--- frontend/libs/ui/pages/src/lib/user/user-list/user-list.component.ts
+++ frontend/libs/ui/pages/src/lib/user/user-list/user-list.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { User } from '@lectoraat-smart-energy/shared';
+import { Observable } from 'rxjs';
+import { UserService } from '../user.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'lectoraat-smart-energy-user-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
+  providers: [UserService],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css',
 })
-export class UserListComponent {}
+export class UserListComponent implements OnInit {
+  users$!: Observable<User[]>;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.users$ = this.userService.getUsers();
+    this.users$.subscribe((users) => {
+      console.log(users);
+    });
+  }
+}
