cd358b78a14450fe31472606759c337feacc8d4e
Job79 <job@plabble.org>
2023-12-20 16:18:18 +0100

    feat: add auth guard

--- frontend/apps/lectoraat-smart-energy-web/src/app/app.config.ts
+++ frontend/apps/lectoraat-smart-energy-web/src/app/app.config.ts
@@ -4,6 +4,7 @@ import { appRoutes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { IPocketBase, environment } from '@lectoraat-smart-energy/shared';
 import PocketBase from 'pocketbase';
+import { AuthService } from '@lectoraat-smart-energy/ui/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +13,7 @@ export const appConfig: ApplicationConfig = {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    AuthService,
     {
       provide: 'pocketbase',
       useValue: new PocketBase(environment.pocketbase) as IPocketBase,
