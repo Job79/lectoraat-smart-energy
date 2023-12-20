dc23457f81b79110f3c861ba8f6af513360d8072
mhaddouch <m.haddouch1@student.avans.nl>
2023-12-20 10:25:31 +0100

    chore:updated location-detail-list

--- frontend/libs/shared/src/lib/env/environment.ts
+++ frontend/libs/shared/src/lib/env/environment.ts
@@ -2,5 +2,5 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
   production: false,
-  pocketbase: 'http://localhost:8090',
+  pocketbase: 'http://127.0.0.1:8090',
 };
