44a855853197bcc1ca3b4372789d5bb82936b96e
Ivan van Dijk <117907237+ibvandijk@users.noreply.github.com>
2023-12-20 11:12:45 +0100

    added colours to the energy selection

--- frontend/libs/ui/pages/src/lib/calculator/boiler/boiler.component.ts
+++ frontend/libs/ui/pages/src/lib/calculator/boiler/boiler.component.ts
@@ -18,8 +18,18 @@ import { LocationService } from '../../location/location.service';
 export class BoilerComponent implements OnInit {
   boiler: Boiler = {} as Boiler;
   locations$!: Observable<Location[]>;
-
   energyLabels = Object.keys(EnergyLabels);
+
+  energyLabelColors: { [key: string]: { color: string } } = {
+    'a+': { color: 'green' },
+    'a': { color: '#FFD700' }, //yellow was too light, darkyellow too dark so the hexcode #FFD700 will be used.
+    'b': { color: 'orange' },
+    'c': { color: 'red' },
+    'd': { color: 'purple' },
+    'e': { color: 'blue' },
+    'f': { color: 'indigo' },
+    'g': { color: 'violet' },
+  };
 
   constructor(
     private boilerService: BoilerService,
