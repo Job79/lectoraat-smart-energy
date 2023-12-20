9d4761251bea7d53cbfbd41b423a4f45d3342b27
Ivan van Dijk <117907237+ibvandijk@users.noreply.github.com>
2023-12-20 11:51:11 +0100

    Update energyLabels.ts
    remove large commented code

--- frontend/libs/shared/src/lib/models/energyLabels.ts
+++ frontend/libs/shared/src/lib/models/energyLabels.ts
@@ -11,16 +11,5 @@ export const EnergyLabels = {
 
 export type EnergyLabel = keyof typeof EnergyLabels;
 
-//export const EnergyLabels = {
-//  'A+': { calculate: (liter: number) => 5.5 + 3.16 * Math.pow(liter, 0.4), color: 'green' },
-//  'A': { calculate: (liter: number) => 8.5 + 4.25 * Math.pow(liter, 0.4), color: 'yellow' },
-//  'B': { calculate: (liter: number) => 12 + 5.93 * Math.pow(liter, 0.4), color: 'orange' },
-//  'C': { calculate: (liter: number) => 16.66 + 8.33 * Math.pow(liter, 0.4), color: 'red' },
-//  'D': { calculate: (liter: number) => 21 + 10.33 * Math.pow(liter, 0.4), color: 'purple' },
-//  'E': { calculate: (liter: number) => 26 + 13.66 * Math.pow(liter, 0.4), color: 'blue' },
-//  'F': { calculate: (liter: number) => 31 + 16.66 * Math.pow(liter, 0.4), color: 'indigo' },
-//  'G': { calculate: (liter: number) => 31 + 16.66 * Math.pow(liter, 0.4), color: 'violet' },
-//};
-
 
 
