979793b2ef91b4cb553170787dc174757cb989a7
studentThomas <twa.vermeulen@student.avans.nl>
2023-12-20 12:09:04 +0100

    fix: add save boiler

--- frontend/libs/ui/pages/src/lib/calculator/boiler/boiler.component.ts
+++ frontend/libs/ui/pages/src/lib/calculator/boiler/boiler.component.ts
@@ -1,23 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoilerService } from '../boiler.service';
 import { FormsModule } from '@angular/forms';
-import { Boiler, EnergyLabels } from '@lectoraat-smart-energy/shared';
+import { Boiler, EnergyLabels, Location } from '@lectoraat-smart-energy/shared';
 import { BoilerResultComponent } from '@lectoraat-smart-energy/ui/components';
+import { Observable } from 'rxjs';
+import { LocationService } from '../../location/location.service';
 
 @Component({
   selector: 'lectoraat-smart-energy-boiler',
   standalone: true,
   imports: [CommonModule, FormsModule, BoilerResultComponent],
-  providers: [BoilerService],
+  providers: [BoilerService, LocationService],
   templateUrl: './boiler.component.html',
   styleUrl: './boiler.component.css',
 })
-export class BoilerComponent {
+export class BoilerComponent implements OnInit {
   boiler: Boiler = {} as Boiler;
+  locations$!: Observable<Location[]>;
+
   energyLabels = Object.keys(EnergyLabels);
 
-  constructor(private boilerService: BoilerService) {}
+  constructor(
+    private boilerService: BoilerService,
+    private locationService: LocationService,
+  ) {}
+
+  ngOnInit(): void {
+    this.locations$ = this.locationService.getLocations();
+  }
 
   storeCalculation() {
     this.boilerService.storeCalculation(this.boiler);
