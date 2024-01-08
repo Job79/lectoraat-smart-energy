49ce5879a960254f81682ac5e86d438616ee97bf
studentThomas <twa.vermeulen@student.avans.nl>
2024-01-08 10:49:17 +0100

    feat: add mobile navbar

--- frontend/libs/features/settings/src/pages/settings/settings.component.ts
+++ frontend/libs/features/settings/src/pages/settings/settings.component.ts
@@ -4,12 +4,20 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { themeChange } from 'theme-change';
 import { FormsModule } from '@angular/forms';
 import { SettingsNavbarComponent } from '../../components/settings-navbar/settings-navbar.component';
+import { SettingsNavbarMobileComponent } from '../../components/settings-navbar-mobile/settings-navbar-mobile.component';
 themeChange();
 
 @Component({
   selector: 'smart-energy-settings',
   standalone: true,
-  imports: [CommonModule, RouterModule, RouterOutlet, FormsModule, SettingsNavbarComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    RouterOutlet,
+    FormsModule,
+    SettingsNavbarComponent,
+    SettingsNavbarMobileComponent,
+  ],
   templateUrl: './settings.component.html',
 })
 export class SettingsComponent {}
