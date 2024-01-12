9e41470f86c39eda1454bec6d0df0529f1a91eac
studentThomas <twa.vermeulen@student.avans.nl>
2024-01-12 14:41:00 +0100

    feat: add boiler test

--- frontend/apps/smart-energy-web-e2e/src/e2e/app.cy.ts
+++ frontend/apps/smart-energy-web-e2e/src/e2e/app.cy.ts
@@ -1,3 +1,15 @@
 describe('smart-energy-web-e2e', () => {
-  beforeEach(() => cy.visit('/'));
+  it('boiler calculation should be correct', () => {
+    cy.visit('calculators/boiler');
+    cy.get('input[name="liter"]').type('10');
+    cy.get('input[name="pricePerKwh"]').type('10');
+    cy.get('input[name="power"]').type('10');
+    cy.get('select[name="energyLabel"]').select('A');
+
+    cy.get('[result] .stat-value').eq(0).should('have.text', '€ 6.4');
+    cy.get('[result] .stat-value').eq(1).should('have.text', '64 Uur');
+    cy.get('[result] .stat-value').eq(2).should('have.text', '0.64 kWh');
+    cy.get('[result] .stat-value').eq(3).should('have.text', '€ 167.9');
+    cy.get('[result] .stat-value').eq(4).should('have.text', '0.46 kWh');
+  });
 });
