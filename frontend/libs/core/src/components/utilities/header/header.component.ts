1348ce0dc7b42006dd25383669c3b22a29164612
Job79 <job@plabble.org>
2024-01-14 20:53:44 +0100

    refactor: move icon styling to header component

--- frontend/libs/core/src/components/utilities/header/header.component.ts
+++ frontend/libs/core/src/components/utilities/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { IconUserComponent } from '../../icons/icon-user/icon-user.component';
 
 @Component({
   selector: 'smart-energy-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, IconUserComponent],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
