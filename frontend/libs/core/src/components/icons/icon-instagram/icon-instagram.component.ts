7df88fe3ee8e13cedd7cf24feabdbd99c409bcf6
studentThomas <twa.vermeulen@student.avans.nl>
2024-01-16 15:49:03 +0100

    fix: fixed footer

--- frontend/libs/core/src/components/icons/icon-instagram/icon-instagram.component.ts
+++ frontend/libs/core/src/components/icons/icon-instagram/icon-instagram.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -7,6 +7,4 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './icon-instagram.component.html',
 })
-export class IconInstagramComponent {
-  @Input() color1 = 'fill-primary';
-}
+export class IconInstagramComponent {}
