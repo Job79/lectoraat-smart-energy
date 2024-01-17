91ceae28f3c11cb93e3a08128bef552ea250f067
studentThomas <twa.vermeulen@student.avans.nl>
2024-01-17 09:42:42 +0100

    feat: add browser titles

--- frontend/apps/smart-energy-web/src/app/app.routes.ts
+++ frontend/apps/smart-energy-web/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Route } from '@angular/router';
 import { IsLoggedIn, IsManager, LoginComponent, SetupAccountComponent } from '@smart-energy/core';
 
+const appName = 'Rekentool | ';
 export const appRoutes: Route[] = [
   {
     path: '',
@@ -11,28 +12,34 @@ export const appRoutes: Route[] = [
     path: 'login',
     pathMatch: 'full',
     component: LoginComponent,
+    title: `${appName}Login`,
   },
   {
     path: 'setup-account',
     component: SetupAccountComponent,
+    title: `${appName}Activatie`,
   },
   {
     path: 'calculators',
     loadChildren: () => import('@smart-energy/calculators').then((m) => m.calculatorRoutes),
+    title: `${appName}Overzicht`,
   },
   {
     path: 'locations',
     canActivate: [IsLoggedIn],
     loadChildren: () => import('@smart-energy/locations').then((m) => m.locationRoutes),
+    title: `${appName}Locaties`,
   },
   {
     path: 'settings',
     canActivate: [IsLoggedIn],
     loadChildren: () => import('@smart-energy/settings').then((m) => m.settingRoutes),
+    title: `${appName}Instellingen`,
   },
   {
     path: 'users',
     canActivate: [IsLoggedIn, IsManager],
     loadChildren: () => import('@smart-energy/users').then((m) => m.userRoutes),
+    title: `${appName}Gebruikers`,
   },
 ];
