658665060471c880ee606ef2d5b3af65b531be20
studentThomas <twa.vermeulen@student.avans.nl>
2024-01-18 18:32:33 +0100

    fix: made text to equal

--- frontend/libs/features/calculators/src/pages/calculators/boiler/boiler.formula.spec.ts
+++ frontend/libs/features/calculators/src/pages/calculators/boiler/boiler.formula.spec.ts
@@ -1,35 +1,35 @@
-import { BoilerFormula, IBoilerParameters, EnergyLabels } from './boiler.formula';
+import { BoilerFormula, IBoilerParameters } from './boiler.formula';
 
 describe('BoilerFormula', () => {
-    let boilerFormula: BoilerFormula;
+  let boilerFormula: BoilerFormula;
 
-    beforeEach(() => {
-        const params: IBoilerParameters = {
-            pricePerKwh: 0.1,
-            capacity: 100,
-            power: 2000,
-            energyLabel: 'A+'
-        };
-        boilerFormula = new BoilerFormula(params);
-    });
+  beforeEach(() => {
+    const params: IBoilerParameters = {
+      pricePerKwh: 0.1,
+      capacity: 100,
+      power: 2000,
+      energyLabel: 'A+',
+    };
+    boilerFormula = new BoilerFormula(params);
+  });
 
-    it('should calculate cost of heating once', () => {
-        expect(boilerFormula.costHeatingOnce).toBeCloseTo(0.64, 2);
-    });
+  it('should calculate cost of heating once', () => {
+    expect(boilerFormula.costHeatingOnce).toEqual(0.64);
+  });
 
-    it('should calculate time required for heating', () => {
-        expect(boilerFormula.timeRequiredForHeating).toBeCloseTo(3.2, 2);
-    });
+  it('should calculate time required for heating', () => {
+    expect(boilerFormula.timeRequiredForHeating).toEqual(3.2);
+  });
 
-    it('should calculate energy required for heating', () => {
-        expect(boilerFormula.energyRequiredForHeating).toBeCloseTo(6.4, 2);
-    });
+  it('should calculate energy required for heating', () => {
+    expect(boilerFormula.energyRequiredForHeating).toEqual(6.4);
+  });
 
-    it('should calculate costs of energy loss per year', () => {
-        expect(boilerFormula.costsEnergyLossPerYear).toBeCloseTo(222.65, 2);
-    });
+  it('should calculate costs of energy loss per year', () => {
+    expect(boilerFormula.costsEnergyLossPerYear).toEqual(222.65);
+  });
 
-    it('should calculate energy loss per day', () => {
-        expect(boilerFormula.energyLossPerDay).toBeCloseTo(0.61, 2);
-    });
+  it('should calculate energy loss per day', () => {
+    expect(boilerFormula.energyLossPerDay).toEqual(0.61);
+  });
 });
